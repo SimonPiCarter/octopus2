05f2cb7c4d607494a37b474fd1faf01ee2a17735
Simon Pierre <simon@DESKTOP-CH6DTUR>
2024-02-17 09:42:03 +0100

    [octopus] added position system

--- src/octopus/src/octopus/components/basic/Position.cc
+++ src/octopus/src/octopus/components/basic/Position.cc
@@ -1,5 +1,7 @@
 #include "Position.hh"
 
+#include "octopus/utils/Grid.hh"
+
 namespace octopus
 {
 template<>
@@ -20,4 +22,25 @@ void set_no_op(Position::Memento &v)
     v.vec.x = 0;
     v.vec.y = 0;
 }
+
+void position_system(Grid &grid_p, flecs::entity e, Position const & p, Velocity &v)
+{
+    size_t old_i = size_t(p.vec.x.to_int());
+    size_t old_j = size_t(p.vec.y.to_int());
+    size_t i = size_t((p.vec.x+v.vec.x).to_int());
+    size_t j = size_t((p.vec.y+v.vec.y).to_int());
+
+    if((old_i != i || old_j != j)
+    && !is_free(grid_p, i, j))
+    {
+        v.vec.x = 0;
+        v.vec.y = 0;
+    }
+    else if(old_i != i || old_j != j)
+    {
+        set(grid_p, old_i, old_j, flecs::entity());
+        set(grid_p, i, j, e);
+    }
 }
+
+} // octopus
--- src/octopus/src/octopus/components/basic/Position.hh
+++ src/octopus/src/octopus/components/basic/Position.hh
@@ -1,11 +1,16 @@
 #pragma once
 
+#include "flecs.h"
+
 #include "octopus/components/generic/Components.hh"
 #include "octopus/utils/Vector.hh"
+
 // Position
 
 namespace octopus
 {
+
+class Grid;
 
 struct Velocity {
 	Vector vec;
@@ -25,5 +30,7 @@ void revert(Position &p, Position::Memento const &v);
 
 template<>
 void set_no_op(Position::Memento &v);
+
+void position_system(Grid &grid_p, flecs::entity e, Position const & p, Velocity &v);
 
 } // octopus
