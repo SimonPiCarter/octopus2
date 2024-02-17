c397a6612485d9a9fcbd2178dad9cf06db1a1642
Simon Pierre <simon@DESKTOP-CH6DTUR>
2024-02-17 09:42:30 +0100

    [exe] added usage of position system

--- src/exe/src/main.cc
+++ src/exe/src/main.cc
@@ -62,23 +62,8 @@ int main(int, char *[]) {
     ecs.system<Position const, Velocity>()
         // .multi_threaded()
         .kind<Iteration>()
-        .each([&target_l, &grid_l](flecs::entity &e, Position const & p, Velocity &v) {
-            size_t old_i = size_t(p.vec.x.to_int());
-            size_t old_j = size_t(p.vec.y.to_int());
-            size_t i = size_t((p.vec.x+v.vec.x).to_int());
-            size_t j = size_t((p.vec.y+v.vec.y).to_int());
-
-            if((old_i != i || old_j != j)
-            && !is_free(grid_l, i, j))
-            {
-                v.vec.x = 0;
-                v.vec.y = 0;
-            }
-            else if(old_i != i || old_j != j)
-            {
-				set(grid_l, old_i, old_j, flecs::entity());
-				set(grid_l, i, j, e);
-            }
+        .each([&grid_l](flecs::entity e, Position const & p, Velocity &v) {
+            position_system(grid_l, e, p, v);
         });
 
     // Create computation pipeline
