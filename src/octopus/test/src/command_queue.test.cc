7543119961659ef6d27387f152578a5213f224ac
SimonPiCarter <xxx@gmail.com>
2024-05-15 09:27:27 +0200

    [test] added real tests

--- src/octopus/test/src/command_queue.test.cc
+++ src/octopus/test/src/command_queue.test.cc
@@ -2,7 +2,10 @@
 
 #include "octopus/commands/queue/CommandQueue.hh"
 
+#include <sstream>
+
 using namespace octopus;
+using vString = std::stringstream;
 
 namespace
 {
@@ -16,15 +19,24 @@ struct Walk : public Command {
 	void set(flecs::entity e) const override { e.set(*this); }
 };
 
-void set_up_walk_systems(flecs::world &ecs)
+struct Attack : public Command {
+	Attack() = default;
+	Attack(uint32_t a) : t(a) {}
+	uint32_t t = 0;
+
+	virtual char const * const naming() const override { return "attack"; }
+	virtual void set(flecs::entity e) const { e.set(*this); }
+};
+
+void set_up_walk_systems(flecs::world &ecs, vString &res)
 {
 	// Walk : walk for 7 progress then done
 	ecs.system<Walk, CommandQueue>()
 		.kind(flecs::OnValidate)
 		.with(CommandQueue::state(ecs), Walk().naming())
-		.each([](flecs::entity& e, Walk &walk_p, CommandQueue &cQueue_p) {
+		.each([&res](flecs::entity& e, Walk &walk_p, CommandQueue &cQueue_p) {
 			++walk_p.t;
-			std::cout<<"\t"<<e.name()<<" walking "<<walk_p.t<<std::endl;
+			res<<" w"<<walk_p.t;
 			if(walk_p.t >= 7)
 			{
 				walk_p.t = 0;
@@ -36,30 +48,21 @@ void set_up_walk_systems(flecs::world &ecs)
 	ecs.system<Walk, CommandQueue>()
 		.kind(flecs::PreUpdate)
 		.with(CommandQueue::cleanup(ecs), Walk().naming())
-		.each([](flecs::entity& e, Walk &walk_p, CommandQueue &cQueue_p) {
-			std::cout<<"\t"<<e.name()<<" cleaning up walk "<<walk_p.t<<std::endl;
+		.each([&res](flecs::entity& e, Walk &walk_p, CommandQueue &cQueue_p) {
+			res<<" cw"<<walk_p.t;
 			walk_p.t = 0;
 		});
 }
 
-struct Attack : public Command {
-	Attack() = default;
-	Attack(uint32_t a) : t(a) {}
-	uint32_t t = 0;
-
-	virtual char const * const naming() const override { return "attack"; }
-	virtual void set(flecs::entity e) const { e.set(*this); }
-};
-
-void set_up_attack_systems(flecs::world &ecs)
+void set_up_attack_systems(flecs::world &ecs, vString &res)
 {
 	// Attack : walk for 12 progress then done
 	ecs.system<Attack, CommandQueue>()
 		.kind(flecs::OnValidate)
 		.with(CommandQueue::state(ecs), Attack().naming())
-		.each([](flecs::entity& e, Attack &attack_p, CommandQueue &cQueue_p) {
+		.each([&res](flecs::entity& e, Attack &attack_p, CommandQueue &cQueue_p) {
 			++attack_p.t;
-			std::cout<<"\t"<<e.name()<<" Attacking "<<attack_p.t<<std::endl;
+			res<<" a"<<attack_p.t;
 			if(attack_p.t >= 12)
 			{
 				attack_p.t = 0;
@@ -71,8 +74,8 @@ void set_up_attack_systems(flecs::world &ecs)
 	ecs.system<Attack, CommandQueue>()
 		.kind(flecs::PreUpdate)
 		.with(CommandQueue::cleanup(ecs), Attack().naming())
-		.each([](flecs::entity& e, Attack &attack_p, CommandQueue &cQueue_p) {
-			std::cout<<"\t"<<e.name()<<" cleaning up Attack "<<attack_p.t<<std::endl;
+		.each([&res](flecs::entity& e, Attack &attack_p, CommandQueue &cQueue_p) {
+			res<<" ca"<<attack_p.t;
 			attack_p.t = 0;
 		});
 }
@@ -85,38 +88,51 @@ void set_up_attack_systems(flecs::world &ecs)
 //////////////////////////////////
 //////////////////////////////////
 
+/// Those test test two commands in the command queue
+/// Attack and Walk
+/// systems associated with those commands write in a string stream each action
+/// We test that the string stream has the expected value with different usage of the
+/// queue
+
 TEST(command_queue, simple)
 {
+	vString res;
 	flecs::world ecs;
 	ecs.entity(Walk().naming());
 
 	set_up_command_queue_systems(ecs);
-	set_up_walk_systems(ecs);
+	set_up_walk_systems(ecs, res);
 
 	auto e1 = ecs.entity()
 		.add<CommandQueue>();
 
-
 	for(size_t i = 0 ; i < 10 ; ++ i)
 	{
+		res<<" p"<<i;
 		if(i == 2)
 		{
 			NewCommand cmd_l {{std::shared_ptr<Command>(new Walk(2))}, false, false};
 			e1.set(cmd_l);
 		}
 		ecs.progress();
+		res<<"\n";
 	}
+
+	std::string const ref_l = " p0\n p1\n p2 w3\n p3 w4\n p4 w5\n p5 w6\n p6 w7\n p7 cw0\n p8\n p9\n";
+
+	EXPECT_EQ(ref_l, res.str());
 }
 
 TEST(command_queue, simple_multiple)
 {
+	vString res;
 	flecs::world ecs;
 	ecs.entity(Walk().naming());
 	ecs.entity(Attack().naming());
 
 	set_up_command_queue_systems(ecs);
-	set_up_walk_systems(ecs);
-	set_up_attack_systems(ecs);
+	set_up_walk_systems(ecs, res);
+	set_up_attack_systems(ecs, res);
 
 	auto e1 = ecs.entity()
 		.add<CommandQueue>();
@@ -124,24 +140,31 @@ TEST(command_queue, simple_multiple)
 
 	for(size_t i = 0 ; i < 10 ; ++ i)
 	{
+		res<<" p"<<i;
 		if(i == 2)
 		{
 			NewCommand cmd_l {{std::shared_ptr<Command>(new Walk(5)), std::shared_ptr<Command>(new Attack(0))}, false, false};
 			e1.set(cmd_l);
 		}
 		ecs.progress();
+		res<<"\n";
 	}
+
+	std::string const ref_l = " p0\n p1\n p2 w6\n p3 w7\n p4 cw0 a1\n p5 a2\n p6 a3\n p7 a4\n p8 a5\n p9 a6\n";
+
+	EXPECT_EQ(ref_l, res.str());
 }
 
 TEST(command_queue, simple_multiple_queuing_front)
 {
+	vString res;
 	flecs::world ecs;
 	ecs.entity(Walk().naming());
 	ecs.entity(Attack().naming());
 
 	set_up_command_queue_systems(ecs);
-	set_up_walk_systems(ecs);
-	set_up_attack_systems(ecs);
+	set_up_walk_systems(ecs, res);
+	set_up_attack_systems(ecs, res);
 
 	auto e1 = ecs.entity()
 		.add<CommandQueue>();
@@ -149,6 +172,7 @@ TEST(command_queue, simple_multiple_queuing_front)
 
 	for(size_t i = 0 ; i < 10 ; ++ i)
 	{
+		res<<" p"<<i;
 		if(i == 2)
 		{
 			NewCommand cmd_l {{std::shared_ptr<Command>(new Walk(4)), std::shared_ptr<Command>(new Attack(0))}, false, false};
@@ -160,18 +184,24 @@ TEST(command_queue, simple_multiple_queuing_front)
 			e1.set(cmd_l);
 		}
 		ecs.progress();
+		res<<"\n";
 	}
+
+	std::string const ref_l = " p0\n p1\n p2 w5\n p3 w6\n p4 w7\n p5 cw0 a12\n p6 ca0 a1\n p7 a2\n p8 a3\n p9 a4\n";
+
+	EXPECT_EQ(ref_l, res.str());
 }
 
 TEST(command_queue, simple_multiple_queuing_back)
 {
+	vString res;
 	flecs::world ecs;
 	ecs.entity(Walk().naming());
 	ecs.entity(Attack().naming());
 
 	set_up_command_queue_systems(ecs);
-	set_up_walk_systems(ecs);
-	set_up_attack_systems(ecs);
+	set_up_walk_systems(ecs, res);
+	set_up_attack_systems(ecs, res);
 
 	auto e1 = ecs.entity()
 		.add<CommandQueue>();
@@ -179,6 +209,7 @@ TEST(command_queue, simple_multiple_queuing_back)
 
 	for(size_t i = 0 ; i < 10 ; ++ i)
 	{
+		res<<" p"<<i;
 		if(i == 2)
 		{
 			NewCommand cmd_l {{std::shared_ptr<Command>(new Walk(4)), std::shared_ptr<Command>(new Attack(10))}, false, false};
@@ -190,18 +221,24 @@ TEST(command_queue, simple_multiple_queuing_back)
 			e1.set(cmd_l);
 		}
 		ecs.progress();
+		res<<"\n";
 	}
+
+	std::string const ref_l = " p0\n p1\n p2 w5\n p3 w6\n p4 w7\n p5 cw0 a11\n p6 a12\n p7 ca0 a1\n p8 a2\n p9 a3\n";
+
+	EXPECT_EQ(ref_l, res.str());
 }
 
 TEST(command_queue, simple_replaced)
 {
+	vString res;
 	flecs::world ecs;
 	ecs.entity(Walk().naming());
 	ecs.entity(Attack().naming());
 
 	set_up_command_queue_systems(ecs);
-	set_up_walk_systems(ecs);
-	set_up_attack_systems(ecs);
+	set_up_walk_systems(ecs, res);
+	set_up_attack_systems(ecs, res);
 
 	auto e1 = ecs.entity()
 		.add<CommandQueue>();
@@ -209,6 +246,7 @@ TEST(command_queue, simple_replaced)
 
 	for(size_t i = 0 ; i < 10 ; ++ i)
 	{
+		res<<" p"<<i;
 		if(i == 2)
 		{
 			NewCommand cmd_l {{std::shared_ptr<Command>(new Walk(2))}, false, false};
@@ -220,5 +258,10 @@ TEST(command_queue, simple_replaced)
 			e1.set(cmd_l);
 		}
 		ecs.progress();
+		res<<"\n";
 	}
+
+	std::string const ref_l = " p0\n p1\n p2 w3\n p3 w4\n p4 w5\n p5 cw5 a2\n p6 a3\n p7 a4\n p8 a5\n p9 a6\n";
+
+	EXPECT_EQ(ref_l, res.str());
 }
--- src/octopus/test/src/command_queue_chaining.test.cc
+++ src/octopus/test/src/command_queue_chaining.test.cc
@@ -3,6 +3,7 @@
 #include "octopus/commands/queue/CommandQueue.hh"
 
 using namespace octopus;
+using vString = std::stringstream;
 
 namespace
 {
@@ -25,15 +26,15 @@ struct Attack : public Command {
 	virtual void set(flecs::entity e) const { e.set(*this); }
 };
 
-void set_up_walk_systems(flecs::world &ecs)
+void set_up_walk_systems(flecs::world &ecs, vString &res)
 {
 	// Walk : walk for 7 progress then done
 	ecs.system<Walk, CommandQueue>()
 		.kind(flecs::OnValidate)
 		.with(CommandQueue::state(ecs), Walk().naming())
-		.each([](flecs::entity& e, Walk &walk_p, CommandQueue &cQueue_p) {
+		.each([&res](flecs::entity& e, Walk &walk_p, CommandQueue &cQueue_p) {
 			++walk_p.t;
-			std::cout<<"\t"<<e.name()<<" walking "<<walk_p.t<<std::endl;
+			res<<" w"<<walk_p.t;
 			if(walk_p.t >= 7)
 			{
 				walk_p.t = 0;
@@ -47,21 +48,21 @@ void set_up_walk_systems(flecs::world &ecs)
 	ecs.system<Walk, CommandQueue>()
 		.kind(flecs::PreUpdate)
 		.with(CommandQueue::cleanup(ecs), Walk().naming())
-		.each([](flecs::entity& e, Walk &walk_p, CommandQueue &cQueue_p) {
-			std::cout<<"\t"<<e.name()<<" cleaning up walk "<<walk_p.t<<std::endl;
+		.each([&res](flecs::entity& e, Walk &walk_p, CommandQueue &cQueue_p) {
+			res<<" cw"<<walk_p.t;
 			walk_p.t = 0;
 		});
 }
 
-void set_up_attack_systems(flecs::world &ecs)
+void set_up_attack_systems(flecs::world &ecs, vString &res)
 {
 	// Attack : walk for 12 progress then done
 	ecs.system<Attack, CommandQueue>()
 		.kind(flecs::OnValidate)
 		.with(CommandQueue::state(ecs), Attack().naming())
-		.each([](flecs::entity& e, Attack &attack_p, CommandQueue &cQueue_p) {
+		.each([&res](flecs::entity& e, Attack &attack_p, CommandQueue &cQueue_p) {
 			++attack_p.t;
-			std::cout<<"\t"<<e.name()<<" Attacking "<<attack_p.t<<std::endl;
+			res<<" a"<<attack_p.t;
 			if(attack_p.t >= 12)
 			{
 				attack_p.t = 0;
@@ -73,8 +74,8 @@ void set_up_attack_systems(flecs::world &ecs)
 	ecs.system<Attack, CommandQueue>()
 		.kind(flecs::PreUpdate)
 		.with(CommandQueue::cleanup(ecs), Attack().naming())
-		.each([](flecs::entity& e, Attack &attack_p, CommandQueue &cQueue_p) {
-			std::cout<<"\t"<<e.name()<<" cleaning up Attack "<<attack_p.t<<std::endl;
+		.each([&res](flecs::entity& e, Attack &attack_p, CommandQueue &cQueue_p) {
+			res<<" ca"<<attack_p.t;
 			attack_p.t = 0;
 		});
 }
@@ -87,15 +88,22 @@ void set_up_attack_systems(flecs::world &ecs)
 //////////////////////////////////
 //////////////////////////////////
 
+/// Those test test two commands in the command queue
+/// Attack and Walk
+/// systems associated with those commands write in a string stream each action
+/// We test that the string stream has the expected value with different usage of the
+/// queue
+
 TEST(command_queue_chaining, simple)
 {
+	vString res;
 	flecs::world ecs;
 	ecs.entity(Walk().naming());
 	ecs.entity(Attack().naming());
 
 	set_up_command_queue_systems(ecs);
-	set_up_walk_systems(ecs);
-	set_up_attack_systems(ecs);
+	set_up_walk_systems(ecs, res);
+	set_up_attack_systems(ecs, res);
 
 	auto e1 = ecs.entity()
 		.add<CommandQueue>();
@@ -103,11 +111,17 @@ TEST(command_queue_chaining, simple)
 
 	for(size_t i = 0 ; i < 10 ; ++ i)
 	{
+		res<<" p"<<i;
 		if(i == 2)
 		{
 			NewCommand cmd_l {{std::shared_ptr<Command>(new Walk(2))}, false, false};
 			e1.set(cmd_l);
 		}
 		ecs.progress();
+		res<<"\n";
 	}
+
+	std::string const ref_l = " p0\n p1\n p2 w3\n p3 w4\n p4 w5\n p5 w6\n p6 w7\n p7 cw0 a1\n p8 a2\n p9 a3\n";
+
+	EXPECT_EQ(ref_l, res.str());
 }
