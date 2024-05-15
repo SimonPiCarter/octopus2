ecb0dde57c53c66c566fe8e8bdfef82b574a4830
SimonPiCarter <xxx@gmail.com>
2024-05-15 10:14:53 +0200

    [octopus] step : added dispatch instead of systems

--- src/octopus/src/octopus/components/step/StepContainer.cc
+++ src/octopus/src/octopus/components/step/StepContainer.cc
@@ -13,40 +13,32 @@ void clear_container(StepContainer &container)
 	container.hitpoints.steps.clear();
 }
 
-void declare_apply_system(flecs::world &ecs, std::vector<StepContainer> &container, ThreadPool &pool)
+void dispatch_apply(std::vector<StepContainer> &container, ThreadPool &pool)
 {
-	ecs.system()
-		.kind<Apply>()
-		.iter([&pool, &container](flecs::iter& it) {
-			for(size_t i = 0 ; i < container.size(); ++ i)
-			{
-				std::vector<std::function<void()>> jobs_l;
+	for(size_t i = 0 ; i < container.size(); ++ i)
+	{
+		std::vector<std::function<void()>> jobs_l;
 
-				jobs_l.push_back([i, &container]() {
-					apply_all(container[i].hitpoints);
-				});
-
-				enqueue_and_wait(pool, jobs_l);
-			}
+		jobs_l.push_back([i, &container]() {
+			apply_all(container[i].hitpoints);
 		});
+
+		enqueue_and_wait(pool, jobs_l);
+	}
 }
 
-void declare_revert_system(flecs::world &ecs, std::vector<StepContainer> &container, ThreadPool &pool)
+void dispatch_revert(std::vector<StepContainer> &container, ThreadPool &pool)
 {
-	ecs.system()
-		.kind<Revert>()
-		.iter([&pool, &container](flecs::iter& it) {
-			for(size_t i = 0 ; i < container.size(); ++ i)
-			{
-				std::vector<std::function<void()>> jobs_l;
+	for(size_t i = 0 ; i < container.size(); ++ i)
+	{
+		std::vector<std::function<void()>> jobs_l;
 
-				jobs_l.push_back([i, &container]() {
-					revert_all(container[i].hitpoints);
-				});
-
-				enqueue_and_wait(pool, jobs_l);
-			}
+		jobs_l.push_back([i, &container]() {
+			revert_all(container[i].hitpoints);
 		});
+
+		enqueue_and_wait(pool, jobs_l);
+	}
 }
 
 } // octopus
--- src/octopus/src/octopus/components/step/StepContainer.hh
+++ src/octopus/src/octopus/components/step/StepContainer.hh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <vector>
+#include "flecs.h"
+
 #include "Step.hh"
 
 #include "octopus/components/basic/HitPoint.hh"
@@ -18,8 +20,7 @@ struct StepContainer
 void reserve(StepContainer &container, size_t size);
 void clear_container(StepContainer &container);
 
-
-void declare_apply_system(flecs::world &ecs, std::vector<StepContainer> &container_p, ThreadPool &pool);
-void declare_revert_system(flecs::world &ecs, std::vector<StepContainer> &container_p, ThreadPool &pool);
+void dispatch_apply(std::vector<StepContainer> &container_p, ThreadPool &pool);
+void dispatch_revert(std::vector<StepContainer> &container_p, ThreadPool &pool);
 
 } // octopus
