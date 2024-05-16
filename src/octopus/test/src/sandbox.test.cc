ea6a3a1df642535056127f191e12e1da6f5acca0
Simon Pierre <simon@DESKTOP-CH6DTUR>
2024-05-16 08:36:33 +0200

    added serial

--- src/octopus/test/src/sandbox.test.cc
+++ src/octopus/test/src/sandbox.test.cc
@@ -2,51 +2,90 @@
 
 #include "flecs.h"
 #include <iostream>
+#include <string>
+#include <vector>
 
-struct State {};
-struct Runn { int a = 12; struct State {}; };
-struct Walk { int x = 4; struct State {}; };
+// This example shows how to serialize a component with std::vectors
+
+struct VectorComponent {
+    std::vector<int32_t> ints;
+    std::vector<std::string> strings;
+};
+
+// Reusable reflection support for std::vector
+template <typename Elem, typename Vector = std::vector<Elem>>
+flecs::opaque<Vector, Elem> std_vector_support(flecs::world& world) {
+    return flecs::opaque<Vector, Elem>()
+        .as_type(world.vector<Elem>())
+
+        // Forward elements of std::vector value to serializer
+        .serialize([](const flecs::serializer *s, const Vector *data) {
+            for (const auto& el : *data) {
+                s->value(el);
+            }
+            return 0;
+        })
+
+        // Return vector count
+        .count([](const Vector *data) {
+            return data->size();
+        })
+
+        // Resize contents of vector
+        .resize([](Vector *data, size_t size) {
+            data->resize(size);
+        })
+
+        // Ensure element exists, return pointer
+        .ensure_element([](Vector *data, size_t elem) {
+            if (data->size() <= elem) {
+                data->resize(elem + 1);
+            }
+
+            return &data->data()[elem];
+        });
+}
 
 TEST(sandbox, test)
 {
-	flecs::world ecs;
+    flecs::world ecs;
 
-	ecs.component<State>().add(flecs::Exclusive);
+    // Register reflection for std::string
+    ecs.component<std::string>()
+        .opaque(flecs::String) // Maps to string
+            .serialize([](const flecs::serializer *s, const std::string *data) {
+                const char *str = data->c_str();
+                return s->value(flecs::String, &str); // Forward to serializer
+            })
+            .assign_string([](std::string* data, const char *value) {
+                *data = value; // Assign new value to std::string
+            });
 
-	auto e1 = ecs.entity("e1");
-	e1.add<State, Runn::State>();
-	e1.add<Runn>();
-	e1.add<Walk>();
-	auto e2 = ecs.entity("e2");
-	e2.add<State, Runn::State>();
-	e2.set<Runn>({1234});
-	e2.set<Walk>({7894});
+    // Register reflection for std::vector<int>
+    ecs.component<std::vector<int>>()
+        .opaque(std_vector_support<int>);
 
-	ecs.system<Runn>()
-		.with<State, Runn::State>()
-		.each([](flecs::entity& e, Runn &) {
-			std::cout<<"found one"<<std::endl;
-		});
+    // Register reflection for std::vector<std::string>
+    ecs.component<std::vector<std::string>>()
+        .opaque(std_vector_support<std::string>);
 
-	// EXPECT_TRUE(nullptr != e1.get_second<State, Runn::State>());
-	// EXPECT_FALSE(nullptr != e1.get_second<State, Walk::State>());
+    // Register component with std::vector members
+    ecs.component<VectorComponent>()
+        .member<std::vector<int>>("ints")
+        .member<std::vector<std::string>>("strings");
 
-	std::cout<<"1"<<std::endl;
-	ecs.progress();
+    // Create value & serialize it to JSON
+    VectorComponent v = {{1, 2, 3}, {"foo", "bar"}};
+    std::cout << ecs.to_json(&v) << std::endl;
 
-	e1.add<State, Walk::State>();
-	// EXPECT_FALSE(nullptr != e1.get_second<State, Runn::State>());
-	// EXPECT_TRUE(nullptr != e1.get_second<State, Walk::State>());
+    // Deserialize new values from JSON into value
+    ecs.from_json(&v,
+        "{\"ints\": [4, 5], \"strings\":[\"hello\", \"flecs\", \"reflection\"]}");
 
-	std::cout<<"2"<<std::endl;
-	ecs.progress();
+    // Serialize again
+    std::cout << ecs.to_json(&v) << std::endl;
 
-	e1.remove<State, Walk::State>();
-	// EXPECT_FALSE(nullptr != e1.get_second<State, Runn::State>());
-	// EXPECT_FALSE(nullptr != e1.get_second<State, Walk::State>());
-
-	std::cout<<"3"<<std::endl;
-	ecs.progress();
-
-	std::cout<<ecs.to_json()<<std::endl;
+    // Output:
+    //   {"ints":[1, 2, 3], "strings":["foo", "bar"]}
+    //   {"ints":[4, 5], "strings":["hello", "flecs", "reflection"]}
 }
