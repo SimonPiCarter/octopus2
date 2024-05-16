19a2cf33bf47f08735c7cd0c27fe0cdc92b2cdf2
SimonPiCarter <xxx@gmail.com>
2024-05-16 09:11:23 +0200

    variant sandbox

--- src/octopus/test/src/sandbox.test.cc
+++ src/octopus/test/src/sandbox.test.cc
@@ -4,45 +4,63 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <variant>
 
-// This example shows how to serialize a component with std::vectors
-
-struct VectorComponent {
-    std::vector<int32_t> ints;
-    std::vector<std::string> strings;
+// Use opaque reflection support to add a computed 'result' member to type
+struct Sum {
+    int32_t a;
+    int32_t b;
 };
 
-// Reusable reflection support for std::vector
-template <typename Elem, typename Vector = std::vector<Elem>>
-flecs::opaque<Vector, Elem> std_vector_support(flecs::world& world) {
-    return flecs::opaque<Vector, Elem>()
-        .as_type(world.vector<Elem>())
+struct Point {
+    float x;
+    float y;
+};
 
-        // Forward elements of std::vector value to serializer
-        .serialize([](const flecs::serializer *s, const Vector *data) {
-            for (const auto& el : *data) {
-                s->value(el);
-            }
+struct Line {
+    Point start;
+    Point stop;
+};
+
+struct Variant {
+	std::variant<Line, Sum> var;
+};
+
+
+// Reusable reflection support for std::vector
+template<typename variant_t>
+void variant_support(flecs::world& world) {
+	world.component<variant_t>()
+        // Serialize as struct
+        .opaque(world.component()
+            .member<Line>("line")
+            .member<Sum>("sum"))
+        // Forward struct members to serializer
+        .serialize([](const flecs::serializer *s, const variant_t *data) {
+			if(std::holds_alternative<Line>(data->var))
+			{
+				s->member("line");
+            	s->value(std::get<Line>(data->var));
+			}
+			else if(std::holds_alternative<Sum>(data->var))
+			{
+				s->member("sum");
+            	s->value(std::get<Sum>(data->var));
+			}
             return 0;
         })
 
-        // Return vector count
-        .count([](const Vector *data) {
-            return data->size();
-        })
-
-        // Resize contents of vector
-        .resize([](Vector *data, size_t size) {
-            data->resize(size);
-        })
-
-        // Ensure element exists, return pointer
-        .ensure_element([](Vector *data, size_t elem) {
-            if (data->size() <= elem) {
-                data->resize(elem + 1);
+        // Return address for requested member
+        .ensure_member([](variant_t *dst, const char *member) -> void* {
+            if (!strcmp(member, "line")) {
+				dst->var = Line();
+                return &std::get<Line>(dst->var);
+            } else if (!strcmp(member, "sum")) {
+				dst->var = Sum();
+                return &std::get<Sum>(dst->var);
+            } else {
+                return nullptr; // We can't serialize into fake result member
             }
-
-            return &data->data()[elem];
         });
 }
 
@@ -50,42 +68,78 @@ TEST(sandbox, test)
 {
     flecs::world ecs;
 
-    // Register reflection for std::string
-    ecs.component<std::string>()
-        .opaque(flecs::String) // Maps to string
-            .serialize([](const flecs::serializer *s, const std::string *data) {
-                const char *str = data->c_str();
-                return s->value(flecs::String, &str); // Forward to serializer
-            })
-            .assign_string([](std::string* data, const char *value) {
-                *data = value; // Assign new value to std::string
-            });
+    // Register components with reflection data
+    ecs.component<Point>()
+        .member<float>("x")
+        .member<float>("y");
 
-    // Register reflection for std::vector<int>
-    ecs.component<std::vector<int>>()
-        .opaque(std_vector_support<int>);
+    ecs.component<Line>()
+        .member<Point>("start")
+        .member<Point>("stop");
 
-    // Register reflection for std::vector<std::string>
-    ecs.component<std::vector<std::string>>()
-        .opaque(std_vector_support<std::string>);
+    // Register serialization support for opaque type
+    ecs.component<Sum>()
+        // Serialize as struct
+        .opaque(ecs.component()
+            .member<int32_t>("a")
+            .member<int32_t>("b"))
 
-    // Register component with std::vector members
-    ecs.component<VectorComponent>()
-        .member<std::vector<int>>("ints")
-        .member<std::vector<std::string>>("strings");
+        // Forward struct members to serializer
+        .serialize([](const flecs::serializer *s, const Sum *data) {
+            s->member("x");
+            s->value(data->a);
+            s->member("y");
+            s->value(data->b);
 
-    // Create value & serialize it to JSON
-    VectorComponent v = {{1, 2, 3}, {"foo", "bar"}};
+            s->member("result");
+            s->value(data->a + data->b); // Serialize fake member
+            return 0;
+        })
+
+        // Return address for requested member
+        .ensure_member([](Sum *dst, const char *member) -> void* {
+            if (!strcmp(member, "a")) {
+                return &dst->a;
+            } else if (!strcmp(member, "b")) {
+                return &dst->b;
+            } else {
+                return &dst->b; // We can't serialize into fake result member
+            }
+        });
+
+	variant_support<Variant>(ecs);
+
+    // Serialize value of Sum to JSON
+    Sum v = {10, 20};
     std::cout << ecs.to_json(&v) << std::endl;
 
-    // Deserialize new values from JSON into value
-    ecs.from_json(&v,
-        "{\"ints\": [4, 5], \"strings\":[\"hello\", \"flecs\", \"reflection\"]}");
+    // Deserialize new value into Sum
+    ecs.from_json(&v, "{\"a\": 20, \"b\": 22}");
 
-    // Serialize again
+    // Serialize value again
     std::cout << ecs.to_json(&v) << std::endl;
 
-    // Output:
-    //   {"ints":[1, 2, 3], "strings":["foo", "bar"]}
-    //   {"ints":[4, 5], "strings":["hello", "flecs", "reflection"]}
+    // Create entity with Line as usual
+    flecs::entity e = ecs.entity()
+        .set<Line>({{10, 20}, {30, 40}});
+
+    // Convert Line component to flecs expression string
+    const Line *ptr = e.get<Line>();
+    std::cout << ecs.to_expr(ptr).c_str() << std::endl;
+
+    Variant var_l;
+	var_l.var = *ptr;
+    std::cout << ecs.to_json(&var_l) << std::endl;
+
+	var_l.var = v;
+    std::cout << ecs.to_json(&var_l) << std::endl;
+
+    ecs.from_json(&var_l, "{\"line\":{\"start\":{\"x\":10, \"y\":20}, \"stop\":{\"x\":30, \"y\":40}}}");
+
+    std::cout << ecs.to_json(&var_l) << std::endl;
+
+    // Output
+    //  {"a":10, "b":20, "result":30}
+    //  {"a":22, "b":20, "result":42}
+    // {start: {x: 10.00, y: 20.00}, stop: {x: 30.00, y: 40.00}}
 }
