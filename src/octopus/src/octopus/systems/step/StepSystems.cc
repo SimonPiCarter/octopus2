7b53953b525a6bec67ecc87326457b8a4815db7e
SimonPiCarter <xxx@gmail.com>
2024-05-17 17:45:50 +0200

    [octopus] removed multi thread

--- src/octopus/src/octopus/systems/step/StepSystems.cc
+++ src/octopus/src/octopus/systems/step/StepSystems.cc
@@ -7,7 +7,6 @@ void set_up_step_systems(flecs::world &ecs, ThreadPool &pool, StepManager &manag
 {
 	// apply steps
 	ecs.system<>()
-		.multi_threaded()
 		.kind(flecs::PostUpdate)
 		.iter([&pool, &manager_p](flecs::iter& it) {
 			dispatch_apply(manager_p.get_last_layer(), pool);
