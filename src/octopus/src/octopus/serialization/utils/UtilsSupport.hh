07f8ce1838195fdea6dc310da07d275aaf3276ed
simon <simon@DESKTOP-AL0PF2L.lan>
2024-09-21 15:18:10 +0200

    added stream to utils

--- src/octopus/src/octopus/serialization/utils/UtilsSupport.hh
+++ src/octopus/src/octopus/serialization/utils/UtilsSupport.hh
@@ -1,10 +1,36 @@
 #pragma once
 
 #include "flecs.h"
-
+#include <iostream>
 namespace octopus
 {
 
 void utils_support(flecs::world& ecs);
+
+template<typename type_t>
+void stream_type(std::ostream &oss, flecs::world &ecs, flecs::entity e, type_t arg)
+{
+	if(e.get<type_t>())
+		oss<<ecs.to_json(e.get<type_t>());
+	else
+		oss<<"null";
+}
+
+template<typename type_t, typename... Targs>
+void stream_type(std::ostream &oss, flecs::world &ecs, flecs::entity e, type_t arg, Targs... Fargs)
+{
+	if(e.get<type_t>())
+		oss<<ecs.to_json(e.get<type_t>())<<", ";
+	else
+		oss<<"null, ";
+	stream_type(oss, ecs, e, Fargs...);
+}
+
+template<typename... Targs>
+void stream_ent(std::ostream &oss, flecs::world &ecs, flecs::entity e)
+{
+	oss<<e.name()<<" : ";
+	stream_type(oss, ecs, e, Targs()...);
+}
 
 } // namespace octopus
