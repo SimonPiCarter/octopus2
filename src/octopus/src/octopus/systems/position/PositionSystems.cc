fbfe6030faa3a5fb8ec5328d5168c20cb51ea50e
simon <simon@DESKTOP-AL0PF2L.lan>
2024-10-16 23:30:58 +0200

    [octopus] position : added some debug logs

--- src/octopus/src/octopus/systems/position/PositionSystems.cc
+++ src/octopus/src/octopus/systems/position/PositionSystems.cc
@@ -20,17 +20,22 @@ Vector separation_force(PositionContext const &posContext_p, Position const &pos
 {
 	Vector force;
 	Fixed force_factor = 500;
+	Logger::getDebug() << "separation_force :: "<<std::endl;
 
 	std::function<bool(int32_t, flecs::entity)> func_l = [&](int32_t idx_l, flecs::entity e) -> bool {
 		Position const *pos_l = e.get<Position>();
+		Logger::getDebug() << "separation_force :: with name=" << e.name()<<" id="<<e.id()<<std::endl;
 		assert(pos_l);
 		if(!pos_l->collision || pos_l->mass == Fixed::Zero())
 		{
+			Logger::getDebug() << "separation_force :: skipped"<<std::endl;
 			return true;
 		}
 		Vector diff = pos_ref_p.pos - pos_l->pos;
 		Fixed length_squared = square_length(diff);
 		Fixed ray_squared = (pos_ref_p.ray + pos_l->ray)*(pos_ref_p.ray + pos_l->ray);
+		Logger::getDebug() << "separation_force :: pos_ref_p.ray ="<<pos_ref_p.ray<<std::endl;
+		Logger::getDebug() << "separation_force :: pos_l->ray ="<<pos_l->ray<<std::endl;
 		Fixed max_range_squared = ray_squared * 2;
 		if(length_squared <= ray_squared && length_squared > 0.001)
 		{
