0dac9d572c502d1053fa7ce7331cf5fe0f8391a0
SimonP <SimonP@gmail.com>
2025-04-17 19:35:41 +0200

    [commands] basic/rally_point : disable rally point when target is too close  to position

--- src/octopus/src/octopus/commands/basic/rally_point/SetRallyPointCommand.hh
+++ src/octopus/src/octopus/commands/basic/rally_point/SetRallyPointCommand.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "octopus/components/basic/rally_point/RallyPoint.hh"
+#include "octopus/components/basic/position/Position.hh"
 
 namespace octopus
 {
@@ -16,11 +17,18 @@ struct SetRallyPointCommand {
 template<class StepManager_t, class CommandQueue_t>
 void set_up_rally_point_command_system(flecs::world &ecs, StepManager_t &manager)
 {
-	ecs.system<SetRallyPointCommand const, CommandQueue_t>()
+	ecs.system<Position const, SetRallyPointCommand const, CommandQueue_t>()
 		.kind(ecs.entity(PostUpdateUnpausedPhase))
 		.with(CommandQueue_t::state(ecs), ecs.component<SetRallyPointCommand::State>())
-		.each([&ecs, &manager](flecs::entity e, SetRallyPointCommand const &rally_point_command, CommandQueue_t &queue) {
-			manager.get_last_layer().back().template get<RallyPointStep>().add_step(e, {rally_point_command.rally_point});
+		.each([&ecs, &manager](flecs::entity e, Position const &pos, SetRallyPointCommand const &rally_point_command, CommandQueue_t &queue) {
+			if(pos.pos == rally_point_command.rally_point.target)
+			{
+				manager.get_last_layer().back().template get<RallyPointStep>().add_step(e, {pos.pos, 0, true});
+			}
+			else
+			{
+				manager.get_last_layer().back().template get<RallyPointStep>().add_step(e, {rally_point_command.rally_point});
+			}
 			queue._queuedActions.push_back(CommandQueueActionDone());
 		});
 }
