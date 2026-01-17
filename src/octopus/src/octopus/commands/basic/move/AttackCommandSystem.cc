b52ed396cb6f024c3ccca024abbf11cc327d6e00
Simon <xxx.yyy.com>
2026-01-17 10:36:42 +0100

    fixed cannot heal itself

--- src/octopus/src/octopus/commands/basic/move/AttackCommandSystem.cc
+++ src/octopus/src/octopus/commands/basic/move/AttackCommandSystem.cc
@@ -34,14 +34,14 @@ flecs::entity get_new_target(flecs::entity const &e, PositionContext const &cont
 	if(can_use_trees_team)
 	{
 		size_t tree_idx = ally ? context_p.trees_team_hp[(team_l->team+1) % 2] : context_p.trees_team_hp[team_l->team];
-		new_candidates_l = get_closest_entities(1, tree_idx, range_p, context_p, pos_p, [ally](flecs::entity const &other_p) -> bool {
-			return health_check_alive(other_p, ally);
+		new_candidates_l = get_closest_entities(1, tree_idx, range_p, context_p, pos_p, [&e, ally](flecs::entity const &other_p) -> bool {
+			return other_p != e && health_check_alive(other_p, ally);
 		});
 	}
 	else
 	{
-		new_candidates_l = get_closest_entities(1, 0, range_p, context_p, pos_p, [team_l, ally](flecs::entity const &other_p) -> bool {
-			if(other_p.try_get<Team>() && health_check_alive(other_p, ally))
+		new_candidates_l = get_closest_entities(1, 0, range_p, context_p, pos_p, [&e, team_l, ally](flecs::entity const &other_p) -> bool {
+			if(other_p != e && other_p.try_get<Team>() && health_check_alive(other_p, ally))
 			{
 				bool is_same_team = team_l->team == other_p.try_get<Team>()->team;
 				// if looking for ally we need the team to be the same
